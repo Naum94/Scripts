9a0e545ec2ec7bc4eab923877a764cb09f284026
Naum94 <36405468+Naum94@users.noreply.github.com>
2022-03-18 15:56:31 +0100

    Update system-check.sh
    Add more functionality and better formating

--- system-check.sh
+++ system-check.sh
@@ -2,15 +2,37 @@
 
 # =========================================================#
 # This script check for system resources and displays them #
-# Author: Naum Ivanovski				   #
+# Author: Naum Ivanovski                                   #
 # =========================================================#
 
 # Get CPU utilization in % (Formula: 100 - idle CPU)
-CPU_INFO=$(top -b -n 1 | grep "Cpu" | awk '{print "CPU Usage: "100-$8"%"}')
+CPU_INFO=$(top -b -n 1 | grep "Cpu" | awk '{print "\tCPU Usage: "100-$8"%"}')
 
 # Get RAM memory utilization (Formula: used/total MB (used/total*100%))
-RAM_INFO=$(free -m | grep "Mem:" | awk '{print "RAM Usage: "$3"/"$2" MB (" $3/$2*100 "%)"}')
+RAM_INFO=$(free -m | grep "Mem:" | awk '{print "\tRAM Usage: "$3"/"$2" MB (" $3/$2*100 "%)"}')
 
-# Display statistics
-echo "${CPU_INFO} | ${RAM_INFO}"
+# Get SWAP memory utilization (Formula: used/total MB (used/total*100%))
+SWAP_INFO=$(free -m | grep "Swap:" | awk '{print "\tSwap Usage: "$3"/"$2" MB (" $3/$2*100 "%)"}')
+
+# Get Disk space
+DISK_INFO=$(df -h | awk '{print "\t" $0}')
+
+#====================# 
+# Display statistics #
+#====================#
+
+clear
+
+echo "Disk space:"
+echo $DISK_INFO
+echo ""
+
+echo "Memory usage:"
+echo $CPU_INFO
+echo $RAM_INFO
+echo $SWAP_INFO
+echo ""
+
+echo -n "Uptime:"
+uptime
 echo ""
